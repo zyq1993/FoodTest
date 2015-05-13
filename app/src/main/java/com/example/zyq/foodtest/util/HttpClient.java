b586cc4308659eab41aca740ab6b522b3d804a2a
Lambdell <speedcell4@gmail.com>
2015-05-14 00:37:14 +0800

    阻塞

--- app/src/main/java/com/example/zyq/foodtest/util/HttpClient.java
+++ app/src/main/java/com/example/zyq/foodtest/util/HttpClient.java
@@ -4,7 +4,11 @@ package com.example.zyq.foodtest.util;
  * Created by ZYQ on 2015/5/4 0004.
  */
 
+import android.graphics.Bitmap;
+
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.util.concurrent.ExecutionException;
 
 public class HttpClient extends DefaultHttpClient {
     private static HttpClient ourInstance = new HttpClient();
@@ -22,5 +26,40 @@ public class HttpClient extends DefaultHttpClient {
     public String getDomain() {
         return domain;
     }
-}
 
+    public String httpGet(String... params) {
+        try {
+            return new Get().execute(params).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Bitmap httpImage(String... params) {
+        try {
+            return new Image().execute(params).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    protected String httpPost(String... params) {
+        try {
+            return new Post().execute(params).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
